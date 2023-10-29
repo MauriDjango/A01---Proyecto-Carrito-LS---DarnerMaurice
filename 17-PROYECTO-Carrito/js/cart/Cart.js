05b622704c82ed2090705936cd50d032c3a31b9c
Maurice Darner <mdar615@g.educaand.es>
2023-10-29 23:40:06 +0100

    First Commit

--- 17-PROYECTO-Carrito/js/cart/Cart.js
+++ 17-PROYECTO-Carrito/js/cart/Cart.js
@@ -1,37 +1,138 @@
-
-/*function loadEventListeners() {}*/
-
+/**
+ * Represents a shopping cart.
+ */
 class Cart {
-  _items = []
+  _items = {};
 
+  /**
+   * Adds one or more items to the cart.
+   * @param {...Object} args - One or more item objects to add to the cart.
+   */
   addItem(...args) {
-    for (let i = 0; i < args.length; i++) {
-      if (args[i].name in this._items) {
-        this._items[args[i].name].quantity++
-      }
-      else {
-        this._items[args[i].name ]= {itemInfo : args[i], quantity : 1}
+    for (const element of args) {
+      if (element.name in this._items) {
+        this._items[element.name].quantity++;
+      } else {
+        this._items[element.name] = { itemInfo: element, quantity: 1 };
       }
     }
   }
 
+  /**
+   * Retrieves the items currently in the cart.
+   * @returns {Object} - An object containing the items in the cart.
+   */
   getItems() {
-    return this._items
+    return this._items;
   }
 
-  removeItem(itemName) {
-    let itemMap = this._items.map((cartItem) => cartItem.name)
-    let itemIndex = itemMap.indexOf(itemName)
-
-    if (itemIndex !== -1)
-    {
-      this._items.slice(itemIndex, itemIndex + 1)
-    }
+  /**
+   * Stores the cart items in the local storage.
+   */
+  storeItems() {
+    localStorage.setItem('items', JSON.stringify(this._items));
   }
 
-  emptyCart () {
-    this._items = []
+  /**
+   * Retrieves the stored cart items from the local storage.
+   * @returns {Object} - An object containing the stored cart items.
+   */
+  getStoredItems() {
+    return JSON.parse(localStorage.getItem('items'));
+  }
+
+  /**
+   * Empties the cart and removes stored items from local storage.
+   */
+  emptyCart() {
+    this._items = {};
+    localStorage.removeItem('items');
+    renderCart(cart);
   }
 }
 
-const cart = new Cart()
+/**
+ * Renders the cart items on the web page.
+ * @param {Cart} cart - The shopping cart to render.
+ */
+export function renderCart(cart) {
+  const cartTableBody = document.getElementById("cart-table-body");
+  cartTableBody.innerHTML = ''; // Clear the existing content
+
+  const items = cart.getStoredItems();
+
+  for (let item in items) {
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+      <td><img src="${items[item].itemInfo.image}" alt="Course Image" class="cart-image"></td>
+      <td>${items[item].itemInfo.name}</td>
+      <td>${items[item].itemInfo.price}</td>
+      <td>${items[item].quantity}</td>
+    `;
+
+    cartTableBody.appendChild(row);
+  }
+}
+
+/**
+ * Represents a utility class to create course items.
+ */
+export class CreateCourseItem {
+  /**
+   * Creates a course item from the provided info card.
+   * @param {Element} infoCard - The info card element.
+   * @returns {Object} - An object representing the course item.
+   */
+  static createCourseItem(infoCard) {
+    return {
+      image: this.getImage(infoCard),
+      name: this.getName(infoCard),
+      price: this.getPrice(infoCard)
+    };
+  }
+
+  /**
+   * Retrieves the image URL from the info card.
+   * @param {Element} infoCard - The info card element.
+   * @returns {string} - The image URL.
+   */
+  static getImage(infoCard) {
+    let image = infoCard.parentElement.children[0].getAttribute("src");
+    if (image === null) { throw NoImageFound; }
+    return image;
+  }
+
+  /**
+   * Retrieves the name from the info card.
+   * @param {Element} infoCard - The info card element.
+   * @returns {string} - The name.
+   */
+  static getName(infoCard) {
+    let name = infoCard.children[0].innerHTML;
+    if (name === null) { throw NoNameFound; }
+    return name;
+  }
+
+  /**
+   * Retrieves the price from the info card.
+   * @param {Element} infoCard - The info card element.
+   * @returns {string} - The price.
+   */
+  static getPrice(infoCard) {
+    const priceElement = infoCard.children[3];
+    let price = null;
+    if (priceElement.childElementCount === 1) {
+      price = priceElement.children[0].innerText;
+    } else {
+      price = priceElement.innerText.split('\n')[0];
+    }
+    if (price === null) { throw NoPriceFound; }
+    return price;
+  }
+}
+
+/**
+ * The shopping cart instance.
+ */
+export const cart = new Cart();
--- 17-PROYECTO-Carrito/js/productPage/ProductPage.js
+++ 17-PROYECTO-Carrito/js/productPage/ProductPage.js
@@ -1,42 +1,59 @@
-import logger from '../Loggers'
+import { cart, CreateCourseItem, renderCart } from "../cart/Cart.js"
 
-loadEventListeners()
+/**
+ * Event listener for the 'DOMContentLoaded' event.
+ * Initializes event listeners when the DOM content is loaded.
+ */
+document.addEventListener("DOMContentLoaded", function() {
+  loadEventListeners();
+})
 
+/**
+ * Initialize event listeners for click and mouseover events.
+ */
 function loadEventListeners() {
   document.addEventListener('click', (e) => {
-    logger.info(`ProductPage click eventListener. Target: ${e.target}`)
-    switch (e.target) {}
-    courseListClickHandler(e)
+    courseListClickHandler(e);
   })
   document.addEventListener('mouseover', (e) => {
-    courseListMouseOverHandler(e)
+    courseListMouseOverHandler(e);
   })
 }
 
+/**
+ * Handle click events on course list items.
+ * @param {Event} e - The click event object.
+ */
 function courseListClickHandler(e) {
   switch (e.target.className) {
-    case "u-full-width button-primary button input agregar-carrito" : {
-      return addCourseToCart(e.target.parentElement)
+    case "u-full-width button-primary button input agregar-carrito": {
+      return addCourseToCart(e.target.parentElement);
     }
-    case "vaciar-carrito.button.u-full-width" : {
-      cart.emptyCart()
-      break
+    case "button u-full-width": {
+      cart.emptyCart();
+      break;
     }
-    default : {}
   }
 }
 
+/**
+ * Handle mouseover events on course list items.
+ * @param {Event} e - The mouseover event object.
+ */
 function courseListMouseOverHandler(e) {
   switch (e.target.className) {
-    case "submenu" : {
-      console.log("Correct mouseover target")
-      cartToList(cart, cartTableBody)
+    case "submenu": {
+      renderCart(cart);
     }
   }
 }
 
-//anadir button card location = e.parentElement.parentElement
+/**
+ * Add a course to the shopping cart and display an alert.
+ * @param {Element} infoCard - The course info card.
+ */
 function addCourseToCart(infoCard) {
-  cart.addItem(CreateCourseItem.createCourseItem(infoCard))
+  cart.addItem(CreateCourseItem.createCourseItem(infoCard));
+  cart.storeItems();
+  alert("Your item has been successfully added to the cart!");
 }
-
